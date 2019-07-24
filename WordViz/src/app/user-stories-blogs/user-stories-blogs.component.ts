9e462c5f33dec1873b16aa5aa6e989837d05f4ad
ArturoOrtizG559 <38798388+ArturoOrtizG559@users.noreply.github.com>
2019-07-24 15:55:56 -0700

    just saving to pull from master

---
+++ WordViz/src/app/user-stories-blogs/user-stories-blogs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-stories-blogs',
+  templateUrl: './user-stories-blogs.component.html',
+  styleUrls: ['./user-stories-blogs.component.css']
+})
+export class UserStoriesBLogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
