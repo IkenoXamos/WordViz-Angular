186ed794bcc1cf3c001eea72d59e4f7c7834ecee
Thomas Payton <thomaspayton53@gmail.com>
2019-07-31 16:45:24 -0500

    added ability to search by tags

--- WordViz/src/app/pipes/SearchPipe.ts
+++ WordViz/src/app/pipes/SearchPipe.ts
@@ -1,11 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Chapter } from '../models/chapter';
+import { Tag } from '../models/tag';
 
 @Pipe({
   name: 'searchFilter'
 })
 export class SearchPipe implements PipeTransform {
   constructor() { }
+  tags:Tag[]=[];
+
   transform(value: any, args?: string): any {
     if (!args) {
       return value;
@@ -13,19 +16,32 @@ export class SearchPipe implements PipeTransform {
     return value.filter((c: Chapter) => {
         let include: boolean = false;
         for(let key in c) {
+            this.tags= [];
             let val: string = '';
             let val2: string = '';
             let val3: string = '';
 
+
             
             if(key == 'story' ) {
-            val = c[key].author.displayName;
-            val2 = c[key].name;
+              val = c[key].author.displayName;
+              val2 = c[key].name;
+              this.tags = c[key].tags;
+              console.log(c[key])
             }
             if(key == 'name'){
                 val3= c[key];     
             }
 
+
+            //search by tag
+            for(let tag of this.tags){
+              if(tag.name.toLowerCase().includes(args.toLowerCase())){
+                include = true;
+                break;
+              }
+            }
+            
             //searh by author name
             if(val.toLowerCase().includes(args.toLowerCase())) {
                 include = true;
