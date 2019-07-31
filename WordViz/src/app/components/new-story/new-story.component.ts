a1a3fe219b20b027941901515167ef67667b8e3d
Thomas Payton <thomaspayton53@gmail.com>
2019-07-31 11:15:55 -0500

    added tag service

--- WordViz/src/app/components/new-story/new-story.component.ts
+++ WordViz/src/app/components/new-story/new-story.component.ts
@@ -3,6 +3,8 @@ import { Story } from 'src/app/models/story';
 import { AuthService } from 'src/app/services/auth.service';
 import { StoryService } from 'src/app/services/story.service';
 import { Router } from '@angular/router';
+import { TagService } from 'src/app/services/tag.service';
+import { Tag } from 'src/app/models/tag';
 
 @Component({
   selector: 'app-new-story',
@@ -13,10 +15,20 @@ export class NewStoryComponent implements OnInit {
   storyTitle:string= '';
   story:Story;
 
+  tags:Tag[];
+  length:number;
+
   constructor(private auth: AuthService,private storyService: StoryService,
-    private router:Router) { }
+    private router:Router, private tagService: TagService) { }
 
   ngOnInit() {
+    this.tagService.getStoryTags().subscribe(
+      data =>{
+        this.tags = data.sort(function(a, b) {return b.tagId - a.tagId });
+        this.length = this.tags.length/2
+
+
+      });
   }
 
   createStory(){
@@ -29,10 +41,7 @@ export class NewStoryComponent implements OnInit {
         else{
           console.log("error creating new story")
         }//add an error handler
-      }
-    );
-
-
+      });
   }
 
 }
