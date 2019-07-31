6409014e285bd513ddefa89178bbe1b32a1940fa
Thomas Payton <thomaspayton53@gmail.com>
2019-07-31 14:22:19 -0500

    updated pipe to git rid of &...;

--- WordViz/src/app/pipes/HtmlPipe.ts
+++ WordViz/src/app/pipes/HtmlPipe.ts
@@ -5,9 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class HtmlPipe implements PipeTransform {
+value2:string;
+value3:string;
+
     transform(value: string): any {
         if(value != null){
-            return value.toString().replace(/<.*?>/g, '');
+            this.value2 = value.toString().replace(/<.*?>/g, '');
+            this.value3= this.value2.toString().replace(/&.*?;/g, ' ');
+            return this.value3;
         }
         else return value;
     }
